de41a1c45c131d2ab99360e28d04bdc9347c577b
Amanda Nikrant <ajnikrant@gmail.com>
2021-01-31 23:24:59 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,24 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+
+def load_library(emoji_file)
+  final_hash = {}
+  new_file = YAML.load_file(emoji_file)
+
+  final_result=  new_file.each_with_object ({}) do |feeling, final_array|
+        feeling.each do |language|
+            if !final_array[feeling][language]
+            final_array[feeling][language]= []
+            end
+        end
+  end
 end
+
+  # new_file [feeling][language] = emoji
+binding.pry
+
 
 def get_japanese_emoticon
   # code goes here
