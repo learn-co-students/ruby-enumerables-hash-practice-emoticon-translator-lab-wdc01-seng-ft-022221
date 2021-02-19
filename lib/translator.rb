a3904eba4a0891c1b7425255e3cc7f33601d19af
Shafee Rushdan <shafeerushdan@yahoo.com>
2021-02-19 15:59:29 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,27 +1,77 @@
 # require modules here
 #require "pry"
 require "yaml"
-
 def load_library(emojis)
-#binding.pry 
-
-#new_emojis = {"english": {},"japanese": {}}
-
-
+new_emojis = {}
 emoticons = YAML.load_file(emojis)
-
-if emoticons do |emoticons_2|
-  emoticons_2 == {"english": {},"japanese": {}}
+emoticons.each{|key,value|
+  new_emojis[key] = {
+   english: value[0],
+   japanese: value[1]
   
- #binding.pry  
+}}
+#binding.pry
+new_emojis
+end
+
+def emoticon_exist(emojis, emoticon)
+  
+ organized_library = load_library(emojis)
+ exist = false
+ 
+ organized_library.each do |key, value|
+   
+   if organized_library[key][:english] == emoticon || organized_library[key][:japanese] == emoticon
+     
+     exist = true
+     
+   end
+   
  end
-emoticons_2
+  
+  return exist
+  
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(emojis, emoticon)
   # code goes here
+  
+  library = load_library(emojis)
+  
+  if emoticon_exist(emojis, emoticon) == false
+    
+   sorry_message = "Sorry, that emoticon was not found"
+    return  sorry_message
+    
+  end
+  
+  library.each do |key, value|
+  
+    if library[key][:english] == emoticon
+      return library[key][:japanese]
+    end
+    
+  end
+  
 end
 
-def get_english_meaning
+def get_english_meaning(emojis, japanese_emoticon)
   # code goes here
-end
+  sorry_message = "Sorry, that emoticon was not found"
+  library = load_library(emojis)
+  
+  if emoticon_exist(emojis, japanese_emoticon) == false
+    
+ return sorry_message
+  end
+  
+  library.each do |key,value|
+    
+    if library[key][:japanese] == japanese_emoticon
+      
+      return key
+    end
+    
+  end 
+  
+end 
