cd973a57756d744a309fc0075681deaa352121b8
Shafee Rushdan <shafeerushdan@yahoo.com>
2021-02-19 02:16:36 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,21 @@
 # require modules here
+#require "pry"
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(emojis)
+#binding.pry 
+
+#new_emojis = {"english": {},"japanese": {}}
+
+
+emoticons = YAML.load_file(emojis)
+
+if emoticons do |emoticons_2|
+  emoticons_2 == {"english": {},"japanese": {}}
+  
+ #binding.pry  
+ end
+emoticons_2
 end
 
 def get_japanese_emoticon
